8f4afd35195a19626d4a54d2b0160e33d53e0095
Orel Ben Hur <orel199973@gmail.com>
2025-07-18 00:32:15 +0300

    local prefix

--- clients/client-a/dev/us-east-1/networking/main.tf
+++ clients/client-a/dev/us-east-1/networking/main.tf
@@ -1,1 +1,14 @@
 # main.tf for networking
+locals {
+  name-prefix = {
+    environment = var.environment
+    company     = var.company
+  }
+  prefix = "${var.company}-${var.environment}"
+}
+
+module "vpc" {
+    source = "../../../../../modules/vpc"
+    for_each = var.vpc
+    
+}
--- clients/client-a/dev/us-east-1/networking/variables.tf
+++ clients/client-a/dev/us-east-1/networking/variables.tf
@@ -1,1 +1,41 @@
 # variables.tf for networking
+variable "environment" {
+  type = any
+}
+
+variable "company" {
+  type = any
+}
+
+variable "location" {
+  type = any
+}
+
+variable "vpc" {
+  type = map(object({
+    cidr_block = string
+  }))
+}
+
+variable "subnets" {
+  type = map(object({
+    cidr_block = string
+    vpc_name   = string
+    az         = string
+    public     = bool
+  }))
+}
+
+variable "igw" {
+  description = "Map of internet gateways"
+  type = map(object({
+    vpc_name = string
+  }))
+}
+
+variable "public_rt" {
+  description = "Map of public_rt"
+  type = map(object({
+    vpc_name = string
+  }))
+}
